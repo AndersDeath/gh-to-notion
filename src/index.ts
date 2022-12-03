47ece78bf8f266a1cdf11d5dd2a44c34e8f2fd50
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-12-03 14:32:15 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Logger } from './logger';
+import { Client } from '@notionhq/client';
 
 require('dotenv').config();
 
 const logger = new Logger();
 
 logger.info('Application has been started');
+
+// const notion = new Client({ auth: process.env.NOTION_KEY })
+// const databaseId = process.env.NOTION_DATABASE_ID
+
