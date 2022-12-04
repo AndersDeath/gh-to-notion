bf40b2eeabf534b0c97a671537a4cf50f0b54aa7
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-12-04 12:46:28 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import { Logger } from './logger';
 import { Client } from '@notionhq/client';
-// import  from 'node-fetch';
+import axios from 'axios';
 
-const fetch = require('node-fetch');
+
 require('dotenv').config();
 
 const logger = new Logger();
 
 logger.info('Application has been started');
 
-// const notion = new Client({ auth: process.env.NOTION_KEY })
-// const databaseId = process.env.NOTION_DATABASE_ID
-
-const test = async () => {
-    const response = await fetch('https://github.com/');
-    const body = await response.text();
-    console.log(body)
+const getGithubData = async () => {
+    const res =  await axios.get('https://api.github.com/search/repositories?q=user:' + process.env.GH_USERNAME);
+    return res.data;
 }
 
 
-test();
+getGithubData().then((e) => {
+    console.log(e);
+});
