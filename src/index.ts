b8bcbbad86112ddf4a01278a681cd2baa88dc571
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-12-04 13:01:51 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,16 @@ const logger = new Logger();
 logger.info('Application has been started');
 
 const getGithubData = async () => {
-    const res =  await axios.get('https://api.github.com/search/repositories?q=user:' + process.env.GH_USERNAME);
-    return res.data;
+    const res =  await axios.get('https://api.github.com/search/repositories?q=user:' + process.env.GH_USERNAME, {
+        headers: {
+            'Authorization': `token ${process.env.GH_TOKEN}`,
+            'Accept-Encoding': 'application/json',
+        }
+    },);
+    return res;
 }
 
 
 getGithubData().then((e) => {
-    console.log(e);
+    console.log(e.data);
 });
