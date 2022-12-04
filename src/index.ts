55dbc76167de10dc02a896460bebcce6aa7c94a8
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-12-04 16:39:40 +0300

    fetch 100 repos

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const logger = new Logger();
 logger.info('Application has been started');
 
 const getGithubData = async () => {
-    const res =  await axios.get('https://api.github.com/search/repositories?q=user:' + process.env.GH_USERNAME, {
+    const res =  await axios.get('https://api.github.com/search/repositories?q=user:' + process.env.GH_USERNAME +'&per_page=100&page=1', {
         headers: {
             'Authorization': `token ${process.env.GH_TOKEN}`,
             'Accept-Encoding': 'application/json',
@@ -19,7 +19,25 @@ const getGithubData = async () => {
     return res;
 }
 
-
+const res = [];
 getGithubData().then((e) => {
-    console.log(e.data);
+    console.log( )
+    console.log(Object.keys(e.data))
+    console.log(e.data.items.length)
+    e.data.items.forEach(element => {
+        // console.log(element);
+        res.push({
+            url: element.html_url,
+            fork: element.fork,
+            desciption: element.description,
+            language: element.language,
+            archived: element.archived,
+            vidibility: element.visibility,
+            created_at: element.created_at,
+            updated_at: element.updated_at,
+            pushed_at: element.pushed_at,
+        });
+    });
+    // console.log(res);
+    console.log(res.length);
 });
