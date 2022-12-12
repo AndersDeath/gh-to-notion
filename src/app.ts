3a18b44d40668adced6cd6e6a976fd0dfbe89f29
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-12-12 16:48:47 +0400

    refactoring

--- src/app.ts
+++ src/app.ts
@@ -2,77 +2,17 @@
 
 
 import { getGithubData } from './gh';
-import { ntnTitleField, ntnUrlField, ntnRichTextField, ntnMultiSelectField, ntnMultiSelectItem, ntnCheckboxField, ntnDateField } from './ntn';
 import { Logger } from './logger';
-import { Client } from '@notionhq/client';
-const notion = new Client({ auth: process.env.NOTION_KEY })
-
-const databaseId = process.env.NOTION_DATABASE_ID
+import { addNtnItem } from './ntn';
 
 const logger = new Logger();
 
 logger.info('Application has been started');
 
-
 export function App() {
-
-    async function addItem(
-        name,
-        html_url,
-        fork,
-        description,
-        language,
-        archived,
-        visibility,
-        created_at,
-        updated_at,
-        pushed_at
-    ) {
-        try {
-            const response = await notion.pages.create({
-                parent: { database_id: databaseId },
-                properties: {
-                    Title: ntnTitleField(name),
-                    URL: ntnUrlField(html_url),
-                    Description: ntnRichTextField(description, 'no description'),
-                    Language: ntnMultiSelectField([
-                        ntnMultiSelectItem(language, 'no language')
-                    ]),
-                    Fork: ntnCheckboxField(fork),
-                    Archived: ntnCheckboxField(archived),
-                    Visibility: ntnRichTextField(visibility, 'no visibility'),
-                    Created_at: ntnDateField(created_at),
-                    Updated_at: ntnDateField(updated_at),
-                    Pushed_at: ntnDateField(pushed_at),
-                },
-            })
-            console.log(response)
-            console.log("Success! Entry added.")
-        } catch (error) {
-            console.error(error.body)
-        }
-    }
-
-    const res = [];
-
-
-
-    getGithubData().then((e) => {
+   getGithubData().then((e) => {
         e.data.items.forEach(element => {
-            res.push({
-                name: element.name,
-                url: element.html_url,
-                fork: element.fork,
-                desciption: element.description,
-                language: element.language,
-                archived: element.archived,
-                vidibility: element.visibility,
-                created_at: element.created_at,
-                updated_at: element.updated_at,
-                pushed_at: element.pushed_at,
-            });
-
-            addItem(
+            addNtnItem(
                 element.name,
                 element.html_url,
                 element.fork,
@@ -86,5 +26,4 @@ export function App() {
             );
         });
     });
-
 }
--- src/ntn.ts
+++ src/ntn.ts
@@ -1,4 +1,9 @@
 
+import { Client } from '@notionhq/client';
+export const notion = new Client({ auth: process.env.NOTION_KEY })
+
+const databaseId = process.env.NOTION_DATABASE_ID
+
 export const ntnTitleField = (name: string): any => {
     return {
       title:[
@@ -59,3 +64,41 @@ export const ntnTitleField = (name: string): any => {
       }
     }
   }
+
+
+  export async function addNtnItem(
+    name,
+    html_url,
+    fork,
+    description,
+    language,
+    archived,
+    visibility,
+    created_at,
+    updated_at,
+    pushed_at
+) {
+    try {
+        const response = await notion.pages.create({
+            parent: { database_id: databaseId },
+            properties: {
+                Title: ntnTitleField(name),
+                URL: ntnUrlField(html_url),
+                Description: ntnRichTextField(description, 'no description'),
+                Language: ntnMultiSelectField([
+                    ntnMultiSelectItem(language, 'no language')
+                ]),
+                Fork: ntnCheckboxField(fork),
+                Archived: ntnCheckboxField(archived),
+                Visibility: ntnRichTextField(visibility, 'no visibility'),
+                Created_at: ntnDateField(created_at),
+                Updated_at: ntnDateField(updated_at),
+                Pushed_at: ntnDateField(pushed_at),
+            },
+        })
+        console.log(response)
+        console.log("Success! Entry added.")
+    } catch (error) {
+        console.error(error.body)
+    }
+}
