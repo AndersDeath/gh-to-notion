e0e41878dcd08a7e027e386bd703be898c521e0e
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-12-15 13:24:44 +0400

    start writing github interfaces

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,25 @@ import { getGithubData, ghParseData, getGithubDataGroup } from './gh';
 import { Logger } from './logger';
 import { addNtnItem, Item } from './ntn';
 
+interface ghItem {
+    id: number;
+    node_id: string;
+    name: string;
+    full_name: string;
+    private: boolean;
+    owner: any;
+    html_url: string;
+    description: string | null;
+    fork: boolean;
+    url: string;
+    
+}
+interface ghData {
+    total_count: boolean;
+    incomplete_results: boolean;
+    items: ghItem[]
+}
+
 /**
  * Logger instance
  */
@@ -15,27 +34,28 @@ export function App() {
     const perPage: number = 20;
     let pageNumber: number = 1;
     getGithubData(perPage, pageNumber).then((e: any) => {
-        let data: Item[] = [];
-        const totalNumber:number = (e.data.total_count) as number;
-        data = [...data, ...ghParseData(e.data)];
-        getGithubDataGroup(pageNumber + 1, totalNumber, perPage).then((q: any) => {
-            data = [...data, ...q];
-            data.forEach((element: Partial<Item> )=> {
-                addNtnItem(
-                    {
-                        name: element.name,
-                        html_url: element.html_url,
-                        fork: element.fork,
-                        description: element.description,
-                        language: element.language,
-                        archived: element.archived,
-                        visibility: element.visibility,
-                        created_at: element.created_at,
-                        updated_at: element.updated_at,
-                        pushed_at: element.pushed_at
-                    }
-                );
-            });
-        });
+        console.log(e.data.);
+        // let data: Item[] = [];
+        // const totalNumber:number = (e.data.total_count) as number;
+        // data = [...data, ...ghParseData(e.data)];
+        // getGithubDataGroup(pageNumber + 1, totalNumber, perPage).then((q: any) => {
+        //     data = [...data, ...q];
+        //     data.forEach((element: Partial<Item> )=> {
+        //         addNtnItem(
+        //             {
+        //                 name: element.name,
+        //                 html_url: element.html_url,
+        //                 fork: element.fork,
+        //                 description: element.description,
+        //                 language: element.language,
+        //                 archived: element.archived,
+        //                 visibility: element.visibility,
+        //                 created_at: element.created_at,
+        //                 updated_at: element.updated_at,
+        //                 pushed_at: element.pushed_at
+        //             }
+        //         );
+        //     });
+        // });
     });
 }
