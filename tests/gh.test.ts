d91738bd39886b81b8c2162a12ff3c308da2fe51
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-12-16 12:09:56 +0400

    update

--- tests/gh.test.ts
+++ tests/gh.test.ts
@@ -1,4 +1,5 @@
-import { ghAuthHeader, ghUserQuery } from './../src/gh';
+import { Item } from './../src/ntn';
+import { ghAuthHeader, ghUserQuery, ghParseData } from './../src/gh';
 import { describe, expect, test } from '@jest/globals';
 
 describe('GithubMethods', () => {
@@ -7,11 +8,44 @@ describe('GithubMethods', () => {
             'Authorization': `token check`,
             'Accept-Encoding': 'application/json',
         });
-    })
+    });
 
     test('ghUserQuery', () => {
-        expect(ghUserQuery({ username: 'check', perPage: 1, page: 1})).toEqual(
+        expect(ghUserQuery({ username: 'check', perPage: 1, page: 1 })).toEqual(
             `https://api.github.com/search/repositories?q=user:check&per_page=1&page=1`
         );
-    })
+    });
+
+    test('ghParseData', () => {
+        const dataMock = {
+            items: [
+                {
+                    name: 'check',
+                    html_url: 'check',
+                    fork: true,
+                    description: 'check',
+                    language: 'check',
+                    archived: true,
+                    visibility: 'check',
+                    created_at: 'check',
+                    updated_at: 'check',
+                    pushed_at: 'check'
+                }
+            ]
+        };
+        const answerMock = [];
+        expect(ghParseData(dataMock)).toEqual([{
+            name: 'check',
+            html_url: 'check',
+            fork: true,
+            description: 'check',
+            language: 'check',
+            archived: true,
+            visibility: 'check',
+            created_at: 'check',
+            updated_at: 'check',
+            pushed_at: 'check'
+        }]);
+    });
+
 });
