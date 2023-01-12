4d00d9d9d0cfa0fded58401a9c3bd7b75f9969c2
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-01-12 18:52:02 +0400

    update readme

--- src/app.ts
+++ src/app.ts
@@ -21,20 +21,20 @@ export function App() {
         getGithubDataGroup(pageNumber + 1, totalNumber, perPage).then((q: any) => {
             data = [...data, ...q];
             data.forEach((element: Partial<Item> )=> {
-                // addNtnItem(
-                //     {
-                //         name: element.name,
-                //         html_url: element.html_url,
-                //         fork: element.fork,
-                //         description: element.description,
-                //         language: element.language,
-                //         archived: element.archived,
-                //         visibility: element.visibility,
-                //         created_at: element.created_at,
-                //         updated_at: element.updated_at,
-                //         pushed_at: element.pushed_at
-                //     }
-                // );
+                addNtnItem(
+                    {
+                        name: element.name,
+                        html_url: element.html_url,
+                        fork: element.fork,
+                        description: element.description,
+                        language: element.language,
+                        archived: element.archived,
+                        visibility: element.visibility,
+                        created_at: element.created_at,
+                        updated_at: element.updated_at,
+                        pushed_at: element.pushed_at
+                    }
+                );
             });
         });
     });
